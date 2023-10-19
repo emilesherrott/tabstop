baee34658ddb53d91b9fa0c64a5d3634e6b34d61
Emile Sherrott <78656376+emilesherrott@users.noreply.github.com>
2023-10-19 15:36:59 +0100

    Update inject.js

--- tabstop.safariextension/inject.js
+++ tabstop.safariextension/inject.js
@@ -2,7 +2,7 @@ var anchors = document.getElementsByTagName('a');
 for (var i = 0; i < anchors.length; i++)
 {
     var anchor = anchors[i];
-    if ('_blank' == anchor.target)
+    if ('_blank' == anchor.target || '_blank')
     {
         anchor.target = '_self';
     }
